599311f98e5228eb79b4a7d9b6a8ba55fd47b488
Prakhar Mishra <mprakhar35@gmail.com>
2022-09-27 13:29:09 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,73 +1,100 @@
-import React, {useState, Fragment} from 'react';
-import ReactCrop from 'react-image-crop';
+import React, { useState, Fragment } from "react";
+import ReactCrop from "react-image-crop";
 
 function App() {
-  const [playing, setPlaying] = useState(false)
-  const [imgPath, setImgPath] = useState('')
-  const [isCropping, setIsCropping] = useState(false)
-  const [newImgPathBase64, setNewImgPathBase64] = useState('')
+  const [playing, setPlaying] = useState(false);
+  const [imgPath, setImgPath] = useState("");
+  const [isCropping, setIsCropping] = useState(false);
+  const [newImgPathBase64, setNewImgPathBase64] = useState("");
   const [crop, setCrop] = useState({ width: 300, height: 400 });
+  const [horizontaloffset, setHorizontaloffset] = useState(0);
+  // const [rightoffset, setRightoffset] = useState(0);
 
-  const HEIGHT = 400;
-  const WIDTH = HEIGHT / 4 * 3;
+  const HEIGHT = 500;
+  const WIDTH = (HEIGHT / 4) * 3;
 
-  navigator.getUserMedia = ( navigator.getUserMedia ||
+  const handleMoveBoxLeft = () => {
+    setHorizontaloffset((prev) => prev -15);
+  };
+
+  const handleMoveBoxRight = () => {
+    setHorizontaloffset((prev) => prev + 15);
+  };
+
+  navigator.getUserMedia =
+    navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia);
+    navigator.msGetUserMedia;
 
-  const startVideo = _ =>{
-    setIsCropping(false)
-    setImgPath('')
-    setPlaying(true)
+  const startVideo = (_) => {
+    setIsCropping(false);
+    setImgPath("");
+    setPlaying(true);
     navigator.getUserMedia(
       {
-        video: true,
+        video: { width: 1000 },
       },
       (stream) => {
-        let video = document.getElementsByClassName('app__videoFeed')[0];
+        let video = document.getElementsByClassName("app__videoFeed")[0];
         if (video) {
           video.srcObject = stream;
         }
       },
       (err) => console.error(err)
     );
-  }
+  };
 
-  const stopVideo = _ => {
-    setPlaying(false)
-    let video = document.getElementsByClassName('app__videoFeed')[0];
+  const stopVideo = (_) => {
+    setPlaying(false);
+    let video = document.getElementsByClassName("app__videoFeed")[0];
     video.srcObject.getTracks()[0].stop();
-  }
+  };
 
-  const captureVideo = _ => {
+  const captureVideo = (_) => {
     const canvas = document.createElement("CANVAS");
-    var video = document.getElementsByClassName('app__videoFeed')[0];
+    var video = document.getElementsByClassName("app__videoFeed")[0];
     canvas.height = video.videoHeight;
-    canvas.width = video.videoHeight/4*3;
-    canvas.getContext('2d').drawImage(video, (video.videoWidth - (video.videoHeight/4*3))/2, 0, video.videoHeight/4*3, video.videoHeight, 0, 0, video.videoHeight/4*3, video.videoHeight);  
-    canvas.toBlob((blob) => {
-        stopVideo()
-        setImgPath(URL.createObjectURL(blob))
-    }, 'image/jpeg', 0.95);
-}
+    canvas.width = (video.videoHeight / 4) * 3;
+    canvas
+      .getContext("2d")
+      .drawImage(
+        video,
+        (video.videoWidth + horizontaloffset + 15 - (video.videoHeight / 4) * 3) / 2,
+        0,
+        (video.videoHeight / 4) * 3,
+        video.videoHeight,
+        0,
+        0,
+        (video.videoHeight / 4) * 3,
+        video.videoHeight
+      );
+    canvas.toBlob(
+      (blob) => {
+        stopVideo();
+        setImgPath(URL.createObjectURL(blob));
+      },
+      "image/jpeg",
+      0.95
+    );
+  };
 
-const onCropChange = (crop) => {
-  const canvas = document.createElement('canvas')
-  const image = document.createElement('img')
-  image.src = imgPath
-  const pixelRatio = window.devicePixelRatio
-  const scaleX = image.naturalWidth / image.width
-  const scaleY = image.naturalHeight / image.height
-  const ctx = canvas.getContext('2d')
+  const onCropChange = (crop) => {
+    const canvas = document.createElement("canvas");
+    const image = document.createElement("img");
+    image.src = imgPath;
+    const pixelRatio = window.devicePixelRatio;
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    const ctx = canvas.getContext("2d");
 
-  canvas.width = crop.width * pixelRatio * scaleX;
-  canvas.height = crop.height * pixelRatio * scaleY;
+    canvas.width = crop.width * pixelRatio * scaleX;
+    canvas.height = crop.height * pixelRatio * scaleY;
 
-  ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
-  ctx.imageSmoothingQuality = 'high';
+    ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+    ctx.imageSmoothingQuality = "high";
 
-  ctx.drawImage(
+    ctx.drawImage(
       image,
       crop.x * scaleX,
       crop.y * scaleY,
@@ -77,85 +104,114 @@ const onCropChange = (crop) => {
       0,
       crop.width * scaleX,
       crop.height * scaleY
-  );
+    );
 
-  setNewImgPathBase64(canvas.toDataURL("image/jpeg"))
-}
+    setNewImgPathBase64(canvas.toDataURL("image/jpeg"));
+  };
 
   return (
-    <div className='container-fluid'>
-      <div className='row mt-4'>
-        {
-          imgPath &&
-          <div className='col-12 text-center'>
-            <img src={imgPath} height={HEIGHT}/>
+    <div className="container-fluid">
+      <div className="row mt-4">
+        {(imgPath && (
+          <div className="col-12 text-center">
+            <img src={imgPath} height={HEIGHT} />
           </div>
-          ||
-          <div className='col-12 text-center'>
-          {playing && <div style={{
-            border: '2px dotted red',
-            position: 'absolute',
-            height: HEIGHT,
-            width: WIDTH,
-            left: 0,
-            right: 0,
-            marginLeft: 'auto',
-            marginRight: 'auto',
-          }}>
-          </div>}
-         
-          <video
-            style={{border: '1px solid white'}}
-            height={HEIGHT}
-            muted
-            autoPlay
-            className='app__videoFeed'
-          />
-        </div>}
+        )) || (
+          <div className="col-12 text-center">
+            {playing && (
+              <div
+                style={{
+                  border: "0.5px solid black",
+                  position: "absolute",
+                  height: HEIGHT,
+                  width: WIDTH,
+                  left: horizontaloffset,
+                  right: 0,
+                  boxShadow: "0px 0px 0px 2000px rgba(255, 255, 255, 0.504)",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                }}
+              ></div>
+            )}
+
+            <video
+              style={{ border: "1px solid white" }}
+              height={HEIGHT}
+              muted
+              autoPlay
+              className="app__videoFeed"
+            />
+          </div>
+        )}
       </div>
-      <div className='row'>
-        <div className='col-12 text-center mt-4'>
-          { 
-            !playing && 
-            <button className='btn btn-primary' onClick={startVideo}>Start</button>
-            ||
-            <button className='btn btn-success' onClick={captureVideo}>Capture</button>
-          }
-          {
-            !playing && !!imgPath &&
-            <button className='btn btn-warning ml-2' onClick={()=>setIsCropping(true)}>Crop</button>
-          }
+      <div className="row">
+        <div className="col-12 text-center mt-4">
+          {(!playing && (
+            <button className="btn btn-primary" onClick={startVideo}>
+              Start
+            </button>
+          )) || (
+            <>
+              <button className="btn btn-success" onClick={captureVideo}>
+                Capture
+              </button>
+              <button  className="btn btn-success" onClick={handleMoveBoxLeft}>
+                Move Left
+              </button>
+              <button  className="btn btn-success" onClick={handleMoveBoxRight}>
+                Move Right
+              </button>
+            </>
+          )}
+          {!playing && !!imgPath && (
+            <button
+              className="btn btn-warning ml-2"
+              onClick={() => setIsCropping(true)}
+            >
+              Crop
+            </button>
+          )}
         </div>
       </div>
-      {isCropping && 
-      <div className='row'>
-        <div className='col-12 text-center'>
-          <hr/>
-          <h4 className='display-4'>Crop your image below!</h4>
-          <hr/>
-        </div>
-          <div className='col-5 text-center'>
+      {isCropping && (
+        <div className="row">
+          <div className="col-12 text-center">
+            <hr />
+            <h4 className="display-4">Crop your image below!</h4>
+            <hr />
+          </div>
+          <div className="col-5 text-center">
             <ReactCrop
-              src={imgPath} 
+              src={imgPath}
               crop={crop}
               ruleOfThirds
               onComplete={onCropChange}
-              onChange={newCrop => setCrop(newCrop)}
+              onChange={(newCrop) => setCrop(newCrop)}
             />
           </div>
-          <div className='col-2 text-center' style={{paddingTop: '100px' }}>
-            <i className="bi bi-arrow-right-square" style={{fontSize: '4rem', }}></i>
+          <div className="col-2 text-center" style={{ paddingTop: "100px" }}>
+            <i
+              className="bi bi-arrow-right-square"
+              style={{ fontSize: "4rem" }}
+            ></i>
           </div>
-          <div className='col-5 text-center'>
+          <div className="col-5 text-center">
             <div>
-              <img src={newImgPathBase64}/>
+              <img src={newImgPathBase64} />
             </div>
             <div>
-              <a className="btn btn-info mt-2" download="Image.png" href={newImgPathBase64} role="button">Download</a>
+              <a
+                className="btn btn-info mt-2"
+                download="Image.png"
+                href={newImgPathBase64}
+                role="button"
+              >
+                Download
+              </a>
             </div>
           </div>
-        
-      </div>}
+        </div>
+      )}
     </div>
   );
 }
